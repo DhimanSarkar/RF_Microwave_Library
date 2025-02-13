b188ffc197674846c6dae025455b6fa25744b075
Dhiman Sarkar <dhiman.eminem@gmail.com>
2025-02-13 20:12:54 +0530

    TRL

--- de_embedding/test_de_embedding.py
+++ de_embedding/test_de_embedding.py
@@ -11,4 +11,4 @@ import matplotlib
 # y = de_embedding_algorithms.TRL().thru2x(r'.\de_embedding\cal_std\meas\v2\1\THRU1.s2p', export_path=r'.\de_embedding\cal_std\meas\v2\1') 
 # y = de_embedding_algorithms.TRL().thru2x(r'.\de_embedding\cal_std\meas\v2\2\THRU2.s2p', export_path=r'.\de_embedding\cal_std\meas\v2\2') 
 
-y = de_embedding_algorithms.TRL().TRL(r'.\de_embedding\cal_std\meas\focus\thru.s2p',r'.\de_embedding\cal_std\meas\focus\refl.s2p',r'.\de_embedding\cal_std\meas\focus\line.s2p', export_path=r'.\de_embedding\cal_std\meas\focus') 
+y = de_embedding_algorithms.TRL().TRL(r'.\de_embedding\cal_std\meas\TRL\3\thru.s2p',r'.\de_embedding\cal_std\meas\TRL\3\shrt.s2p',r'.\de_embedding\cal_std\meas\TRL\3\line.s2p', export_path=r'.\de_embedding\cal_std\meas\TRL\3') 
